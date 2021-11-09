b4e80f38f2eef89ea546aee83254956e51e352e3
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-11-09 15:32:24 +0700

    ...

--- src/main/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumber.java
+++ src/main/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumber.java
@@ -1,19 +1,23 @@
 package com.axonactive.demo_spring.dojo.warmNumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WarmNumber {
 
-    public boolean isWarmNumber(int warmNumber){
-
-        for (int i = 1; i < warmNumber; i++) {
-
-        if(warmNumber%i == 0){
-            if (warmNumber/i == i+1){
+    public boolean isWarmNumber(int warmNumber) {
+        if (warmNumber < 3) return false;
+        for (int i = 1; i < warmNumber / 2; i++)
+            if (i * (i + 1) == warmNumber)
                 return true;
-            }
-        }
-        }
-
         return false;
+    }
+
+    public List<Integer> getListWarmNumber(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 3; i <= n; i++)
+            if (isWarmNumber(i)) result.add(i);
+        return result;
     }
 
 }
--- src/test/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumberTest.java
+++ src/test/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumberTest.java
@@ -1,20 +1,32 @@
 package com.axonactive.demo_spring.dojo.warmNumber;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class WarmNumberTest {
 
     @Test
-    void testIsWarmNumber(){
+    void testIsWarmNumber() {
         WarmNumber w = new WarmNumber();
+        assertTrue(w.isWarmNumber(20));
+        assertFalse(w.isWarmNumber(10));
+        assertFalse(w.isWarmNumber(2));
+    }
 
-        assertEquals(true,w.isWarmNumber(20));
-        assertEquals(false,w.isWarmNumber(10));
-
+    @Test
+    void getListWarmNumber() {
+        WarmNumber w = new WarmNumber();
+//        assertTrue(w.getListWarmNumber(20).containsAll(List.of( 12, 20)));
+//        List<Integer> emptyList = new ArrayList<>();
+//        assertTrue(w.getListWarmNumber(5).containsAll(emptyList));
+//        assertFalse(w.getListWarmNumber(20).containsAll(List.of(5, 6, 12, 20)));
+        assertEquals(List.of(6, 12, 20), w.getListWarmNumber(20));
+        assertEquals(List.of(), w.getListWarmNumber(5));
     }
 
 }
