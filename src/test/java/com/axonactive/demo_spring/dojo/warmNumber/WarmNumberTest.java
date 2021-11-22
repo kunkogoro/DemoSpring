54ff3dffc684cc0dfe8934aaf602202c670bbe23
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-11-22 09:49:48 +0700

    fix warmNumber

--- src/test/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumberTest.java
+++ src/test/java/com/axonactive/demo_spring/dojo/warmNumber/WarmNumberTest.java
@@ -3,6 +3,7 @@ package com.axonactive.demo_spring.dojo.warmNumber;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,8 +26,13 @@ public class WarmNumberTest {
 //        List<Integer> emptyList = new ArrayList<>();
 //        assertTrue(w.getListWarmNumber(5).containsAll(emptyList));
 //        assertFalse(w.getListWarmNumber(20).containsAll(List.of(5, 6, 12, 20)));
-        assertEquals(List.of(6, 12, 20), w.getListWarmNumber(20));
-        assertEquals(List.of(), w.getListWarmNumber(5));
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        list1.add(6);
+        list1.add(12);
+        list1.add(20);
+        assertEquals(list1, w.getListWarmNumber(20));
+        assertEquals(list2, w.getListWarmNumber(5));
     }
 
 }
